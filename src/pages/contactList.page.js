a1d2bae28afdcf8731891664cb14b902e4f31200
sravuri <sravuri@S-MacBook.local>
2018-02-26 17:52:38 -0800

    contact manager app (in progress) with react +redux

---
+++ src/pages/contactList.page.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import {connect} from 'react-redux';
+import ContactList from "../components/contactList";
+import { fetchContacts } from '../actions/contact-actions';
+
+class ContactListPage extends Component {
+
+  componentDidMount = () => {
+    this.props.fetchContacts();
+  };
+  
+  render() {
+    return (
+      <div>
+        <h1>List of Contacts</h1>
+        <ContactList  contacts={this.props.contacts}/>
+      </div>
+    );
+  }
+}
+function mapStateToProps(state){
+  return {
+    contacts:state.contactStore.contacts
+  }
+}
+export default connect(mapStateToProps, { fetchContacts })(
+  ContactListPage
+);
